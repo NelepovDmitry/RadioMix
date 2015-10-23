fd5d85467667720172facbb51331d71c785820d1
Nelepov Dmitry <nelepovds@gmail.com>
2015-10-23 18:03:28 +0300

    Add Monte Carlo

---
+++ Project/iOS/RadioMix/RadioMix/RMLoadingView.swift
@@ -0,0 +1,21 @@
+//
+//  RMLoadingView.swift
+//  RadioMix
+//
+//  Created by Dmitry Nelepov on 23.10.15.
+//  Copyright © 2015 Nelepova Inna. All rights reserved.
+//
+
+import UIKit
+
+class RMLoadingView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- Project/iOS/RadioMix/RadioMix/ViewController.swift
+++ Project/iOS/RadioMix/RadioMix/ViewController.swift
@@ -10,16 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var radioView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.setupRadioView()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //MARK: Radio View
+    func setupRadioView() {
+        
     }
-
 
 }
 
