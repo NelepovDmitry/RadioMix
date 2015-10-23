420746af21b989dda221b7409b2e3e2ea54284a3
Nelepov Dmitry <nelepovds@gmail.com>
2015-10-23 13:32:36 +0300

    Add Cocoapods

---
+++ Project/iOS/RadioMix/RadioMix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RadioMix
+//
+//  Created by Dmitry Nelepov on 23.10.15.
+//  Copyright © 2015 Nelepova Inna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
