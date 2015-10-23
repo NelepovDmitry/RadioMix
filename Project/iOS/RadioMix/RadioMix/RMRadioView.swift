06b4f1ea1186f5bb0b86803362fa35e7c97329ba
Nelepov Dmitry <nelepovds@gmail.com>
2015-10-23 17:43:22 +0300

    RadioStations file and folders

---
+++ Project/iOS/RadioMix/RadioMix/RMRadioView.swift
@@ -0,0 +1,21 @@
+//
+//  RMRadioView.swift
+//  RadioMix
+//
+//  Created by Dmitry Nelepov on 23.10.15.
+//  Copyright © 2015 Nelepova Inna. All rights reserved.
+//
+
+import UIKit
+
+class RMRadioView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
